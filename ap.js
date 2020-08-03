e1e17dda98818ff1e8704c6c49fa375d9ebabc0b
ekta0092 <69057471+ekta0092@users.noreply.github.com>
2020-08-03 13:21:30 +0530

    Update ap.js

--- ap.js
+++ ap.js
@@ -3,21 +3,21 @@
    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-302N6EZWWDUS-2");
-   aptrinsic(“identify”,
+   aptrinsic("identify",
  {
  //User Fields
-   “id”: “Ekta0092”, // Required for logged in app users
-   “email”: “ektakri@TinDog.com”,
-   “firstName”: “Ekta”,
-   “lastName”: “Kumari”,
-   “signUpDate”: 1522697426479, //unix time in ms
-   “plan” : “gold”, //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
-   “price” : 95.5,
-   “userHash”: “” // optional transient for HMAC identification
+   "id": "Ekta0092", // Required for logged in app users
+   "email": "ektakri@TinDog.com",
+   "firstName": "Ekta",
+   "lastName": "Kumari",
+   "signUpDate": 1522697426479, //unix time in ms
+   "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+   "price" : 95.5,
+   "userHash": "" // optional transient for HMAC identification
  },
  {
  //Account Fields
-   “id”:“001Tindog1234”, //Required
-   “name”:“TinDog Account”,
-   “Program”: “Platinum” // flat custom attributes
+   "id":"001Tindog1234", //Required
+   "name":"TinDog Account",
+   "Program": "Platinum" // flat custom attributes
 });
